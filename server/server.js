f3f3df87b71175b890f8b0f82e2629c80ca35b2d
Tarun Gupta <gupta.tarun200012@gmail.com>
2023-05-30 14:56:53 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -49,14 +49,16 @@ mongoose.connect(URI, {
 })
 
 // if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
+    app.use(express.static('../client/build'))
+    app.get('*', (req, res) => {
+
+        res.sendFile(path.join(__dirname,'../client/build/index.html'))
+    })
 // }
 
 
 const port = process.env.PORT || 5000
 http.listen(port, () => {
     console.log('Server is running on port', port)
+    
 })
